34c050f231aa86b1284a8d9b0e7123ee4107d1d2
Jan Šifrer <jan.sifrer1@gmail.com>
2019-01-28 17:48:43 +0100

    Dodal nekaj novih knjižnic

--- lib/libraries.r
+++ lib/libraries.r
@@ -4,6 +4,10 @@ library(gsubfn)
 library(reshape2)
 library(shiny)
 library(tidyr)
+library(dygraphs)
+library(xts)
+library(tidyverse)
+library(lubridate)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
