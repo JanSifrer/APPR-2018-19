e13a02730ca2768157baaecd2115cb522a9be754
Jan Šifrer <jan.sifrer1@gmail.com>
2019-01-28 16:51:50 +0100

    Lepše napisal kodo.

--- shiny/server.R
+++ shiny/server.R
@@ -2,11 +2,6 @@ library(shiny)
 
 function(input, output) {
   output$Yolo <- renderPlot({
-    k <- kmeans(imf.norm, input$num, nstart=1000)
-    skupina <- data.frame(obcina=imf$obcina, skupina=factor(k$cluster))
-    print(ggplot() + geom_polygon(data=left_join(zemljevid,skupina, 
-                                                 by=c("OB_UIME"="obcina")), 
-                                  aes(x=long, y=lat, group=group, 
-                                      fill=skupina)))
+  zemljevid.slovenije(input$num)
   })
 }
